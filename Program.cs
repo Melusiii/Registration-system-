82870d977f2fa4d1e09f0929d4a2f5e5605f33f0
melusi shanzi <melushanzi32@gmail.com>
2025-03-08 12:00:18 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿namespace Reg_System
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            List<Student> students = new List<Student>();
+            Console.WriteLine("Welcome to the school registration system");
+
+            while (true)    
+            {
+                Console.WriteLine("\nPlease choose an option:");
+                Console.WriteLine("1. Register as new student");
+                Console.WriteLine("2. View all students");
+                Console.WriteLine("3. Exit");
+                string choice= Console .ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        RegisterStudent(students);
+                        break;
+                    case "2":
+                        ViewAllStudents(students);
+                        break;
+                    case "3":
+                        Console.WriteLine("Exiting system");
+                        return;
+                    default:
+                        Console.WriteLine("Invalid choice. Pleaaase try again");
+                        break;
+                }
+               
+            }
+        }static void RegisterStudent(List<Student> students)
+        {
+            Console.WriteLine("\nEnter student name:");
+            string name= Console .ReadLine();
+
+            Console.WriteLine( "Enter student ID:");
+            int studentId=int.Parse(Console .ReadLine());
+
+            Console.WriteLine("Enter course name");
+            string course= Console .ReadLine();
+
+            Student student =new Student(name, studentId, course);
+            students.Add(student);
+
+            Console.WriteLine("student registered successfully!");
+
+        }private static void ViewAllStudents(List<Student> students)
+        {
+            Console.WriteLine("\nlist of all registered students:");
+            foreach (var student in students)
+            {
+                Console.WriteLine(student.ToString());
+            }
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reg_System
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public int StudentId { get; set; }
+        public string Course { get; set; }
+
+        public Student(string name, int studentId, string course)
+        {
+            Name = name;
+            StudentId = studentId;
+            Course = course;
+        }
+        public override string ToString() 
+        {
+            return $"ID: {StudentId}, Name: {Name}, Course: {Course}";
+        }
+    }
+}
